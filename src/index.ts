7c03f0607dc0c12147fb02c83ae36e67116881e9
Gedonist <simulyakr@gmail.com>
2026-06-18 09:42:15 +0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import { Hono } from "hono";
 import { tasksRouter } from "./endpoints/tasks/router";
 import { ContentfulStatusCode } from "hono/utils/http-status";
 import { DummyEndpoint } from "./endpoints/dummyEndpoint";
-import { VkEndpoint } from './endpoints/vk/router';
+import { VkEndpoint } from './endpoints/vk';
 
 // Start a Hono app
 const app = new Hono<{ Bindings: Env }>();
